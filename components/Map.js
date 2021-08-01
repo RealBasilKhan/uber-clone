47449c7eaa8ab311e80e0cae0a1dee482da0e6b3
Basil Khan <basilkhan76@gmail.com>
2021-07-31 20:09:52 -0400

    APIs Added. Working on Map Screen.

---
+++ components/Map.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Map = () => {
+  return (
+    <View>
+      <Text>I'm a map</Text>
+    </View>
+  );
+};
+
+export default Map;
+
+const styles = StyleSheet.create({});
